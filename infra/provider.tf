a145bb8b67e603f85d424ace4e7d58fedad041e4
sush0808 <shivapujesush@gmail.com>
2025-01-31 17:43:05 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+  region                   = "ap-south-1"
+  shared_credentials_files = ["/home/ubuntu/terraform-jenkins/credentials"]
 }
